41364868fcea63c05ef0cbc4f9e29d77b534ae46
Daniel Aguilar <daguilar8749@gmail.com>
2025-08-15 12:04:04 -0700

    adding external sql to begin hosting

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,9 @@ def create_app():
     load_dotenv()
 
     app = Flask(__name__)
+
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "default-secret")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///garden.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     CORS(app)
