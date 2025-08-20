655dc268aa1e02e627e7721ad983984056ad3538
Daniel Aguilar <daguilar8749@gmail.com>
2025-08-20 12:04:58 -0700

    auto updating crop database when starting up app for now

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,34 @@ from .models import db
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate   
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-from .sync_calendar import sync_plants_from_calendar  
+from .utils.sync_calendar import sync_plants_from_calendar
 
 jwt = JWTManager()
-migrate = Migrate() 
+migrate = Migrate()
 
 def create_app():
     load_dotenv()
 
     app = Flask(__name__)
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "default-secret")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # <-- use Neon
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # Neon DB
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    with app.app_context():
-        db.create_all()
-        sync_plants_from_calendar() 
 
     CORS(app)
     jwt.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    with app.app_context():
+        # Run migrations if you want them applied automatically:
+        from flask_migrate import upgrade
+        upgrade()
+
+        # Keep plant data in sync
+        sync_plants_from_calendar()
 
     from .routes import api
     app.register_blueprint(api, url_prefix="/api")
